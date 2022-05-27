7e35bf3729acc8ccc56c9c4c92303ff2fa40dff2
jun <devjun1023@gmail.com>
2022-05-27 11:18:46 +0900

    Feat: 엔티티 추가 매핑
    - Comment, Image 엔티티 추가

---
+++ src/main/java/com/example/airbnb/business/domain/accommodation/Comment.java
@@ -0,0 +1,27 @@
+package com.example.airbnb.business.domain.accommodation;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer commentId;
+
+    private String content;
+
+    private double rating;
+
+    private LocalDateTime createAt;
+    private LocalDateTime lastModifiedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accommodation_id")
+    private Accommodation accommodation;
+}
