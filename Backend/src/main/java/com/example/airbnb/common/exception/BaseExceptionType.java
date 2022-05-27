35e8977d13d6b436548f5f78177cdea2c96e30c0
Jun <92818747+devjun10@users.noreply.github.com>
2022-05-27 22:06:37 +0900

    Feature: 예외 정의
    - Accommodation, Member 예외 정의

--- Backend/src/main/java/com/example/airbnb/common/exception/BaseExceptionType.java
+++ Backend/src/main/java/com/example/airbnb/common/exception/BaseExceptionType.java
@@ -1,5 +1,12 @@
 package com.example.airbnb.common.exception;
 
+import org.springframework.http.HttpStatus;
+
 public interface BaseExceptionType {
+    int getErrorCode();
+
+    String getMessage();
+
+    HttpStatus getHttpStatus();
 
 }
