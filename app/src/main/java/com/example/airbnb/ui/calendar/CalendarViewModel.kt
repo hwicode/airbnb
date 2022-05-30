a1d98f9ae8b07ee36fd94bd7b67d69fc70e4ef5d
ramkid91 <ramkid91@gmail.com>
2022-05-30 16:44:15 +0900

    fix : 첫번째주 두번째 PR 리뷰 반영 - setBackground -> bind로 변경 - CalendarUtil 매직 넘버 -> Constants에 상수처리 - default값 처리 수정 - 레포지토리에서 캘린더 데이터 생성 코드 삭제 -> viewModel에서 처리하는 방향으로 수정예정

--- app/src/main/java/com/example/airbnb/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/airbnb/ui/calendar/CalendarViewModel.kt
@@ -4,12 +4,13 @@ import androidx.lifecycle.ViewModel
 import com.example.airbnb.data.RepositoryImpl
 import com.example.airbnb.domain.Repository
 import com.example.airbnb.domain.model.CalendarDay
+import org.joda.time.LocalDate
 import org.joda.time.LocalDateTime
 
 class CalendarViewModel : ViewModel() {
 
-    private val repository: Repository = RepositoryImpl()
-    fun getCalendarMap(): Map<LocalDateTime, List<CalendarDay>> {
-        return repository.getCalendarMap()
+    //클릭한 날짜 정보 viewModel까지 가져오는 테스트
+    fun saveDate(dateTime: LocalDate){
+        println(dateTime)
     }
 }
--- app/src/main/java/com/example/airbnb/ui/calendar/DayAdapter.kt
+++ app/src/main/java/com/example/airbnb/ui/calendar/DayAdapter.kt
@@ -6,22 +6,28 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.airbnb.databinding.ItemCalendarDayBinding
 import com.example.airbnb.domain.model.CalendarDay
+import org.joda.time.LocalDate
+import org.joda.time.LocalDateTime
 
-class DayAdapter : RecyclerView.Adapter<DayAdapter.ViewHolder>() {
+class DayAdapter(private val itemClick: (selectedDate: LocalDate) -> Unit) : RecyclerView.Adapter<DayAdapter.ViewHolder>() {
 
     private val dayList = mutableListOf<CalendarDay>()
 
     class ViewHolder(private val binding: ItemCalendarDayBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(day: CalendarDay) {
+        fun bind(day: CalendarDay, itemClick: (selectedDate:LocalDate) -> Unit) {
             binding.day = day.day
             if (day.isSelectable) {
                 if (day.isStartDay) {
-                    binding.tvCalendarDay.setBackgroundColor(Color.GREEN)
+                    //시작날짜 UI 처리
                 }
-                binding.tvCalendarDay.setTextColor(Color.BLACK)
+                binding.selectableColor= Color.BLACK
             } else {
-                binding.tvCalendarDay.setTextColor(Color.GRAY)
+                binding.selectableColor=Color.GRAY
             }
+            binding.tvCalendarDay.setOnClickListener {
+                itemClick.invoke(LocalDate(day.year, day.month.toInt(), day.day.toInt()))
+            }
+
         }
     }
 
@@ -31,7 +37,7 @@ class DayAdapter : RecyclerView.Adapter<DayAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(dayList[position])
+        holder.bind(dayList[position], itemClick)
     }
 
     override fun getItemCount(): Int {
@@ -42,4 +48,8 @@ class DayAdapter : RecyclerView.Adapter<DayAdapter.ViewHolder>() {
         this.dayList.addAll(list)
     }
 
+    private fun selectDay(selectedDay:LocalDate){
+
+
+    }
 }
