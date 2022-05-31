4491329eb399d46af60d08b615239202834697a8
hwicode <khc3796@naver.com>
2022-05-31 11:00:46 +0900

    Feat: 커밋 테스트 hwicode/airbnb#33
    - 커밋 테스트
    
    Please enter the commit message for your changes. Lines starting

--- Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationController.java
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationController.java
@@ -1,7 +1,14 @@
 package com.example.airbnb.business.web.controller.reservation;
 
+import com.example.airbnb.business.web.service.reservation.ReservationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+
 }
