11bdbaa24286315d83e1caaf51baaea68bf75454
hwicode <khc3796@naver.com>
2022-05-31 23:45:27 +0900

    Feat: ReservationController 수정 hwicode/airbnb#45
    - ReservationResponse를 DetailedReservationResponse로 이름 수정
    - github 아이디로 예약 리스트를 조회하는 기능 추가

--- Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationController.java
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationController.java
@@ -1,6 +1,7 @@
 package com.example.airbnb.business.web.controller.reservation;
 
-import com.example.airbnb.business.web.controller.reservation.dto.ReservationResponse;
+import com.example.airbnb.business.web.controller.reservation.dto.DetailedReservationResponse;
+import com.example.airbnb.business.web.controller.reservation.dto.ReservationListResponse;
 import com.example.airbnb.business.web.service.reservation.ReservationReadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +17,13 @@ public class ReservationController {
     private final ReservationReadService reservationReadService;
 
     @GetMapping("/{id}")
-    public ReservationResponse searchReservationDetail(@PathVariable("id") Long id) {
+    public DetailedReservationResponse searchDetailedReservation(@PathVariable("id") Long id) {
         return reservationReadService.findByReservationId(id);
+    }
+
+    @GetMapping("/users/{githubId}")
+    public ReservationListResponse searchReservations(@PathVariable String githubId) {
+       return reservationReadService.findReservations(githubId);
     }
 
 }
