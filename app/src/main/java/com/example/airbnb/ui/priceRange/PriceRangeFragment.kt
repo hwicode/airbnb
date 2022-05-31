0f2a7c34b62827a9a2a78eedb984bb0b838c100d
Linus <zerolecture0922@gmail.com>
2022-05-31 14:06:30 +0900

    feat : 가격 그래프 화면 일부 구현

--- app/src/main/java/com/example/airbnb/ui/priceRange/PriceRangeFragment.kt
+++ app/src/main/java/com/example/airbnb/ui/priceRange/PriceRangeFragment.kt
@@ -6,16 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.airbnb.R
+import com.example.airbnb.databinding.FragmentPriceRangeBinding
 
 
 class PriceRangeFragment : Fragment() {
+
+    private lateinit var binding: FragmentPriceRangeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_price_range, container, false)
+        binding = FragmentPriceRangeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.etLowestPrice.setText("안녕")
+    }
 }
