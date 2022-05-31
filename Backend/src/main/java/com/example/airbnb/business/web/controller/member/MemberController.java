bd37792967ae62664af21181b45167498d2e48eb
hwicode <khc3796@naver.com>
2022-06-01 03:17:56 +0900

    Feat: MemberController 기능 추가 hwicode/airbnb#47
    -  위시 리스트를 조회하는 기능 추가

--- Backend/src/main/java/com/example/airbnb/business/web/controller/member/MemberController.java
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/member/MemberController.java
@@ -1,7 +1,22 @@
 package com.example.airbnb.business.web.controller.member;
 
+import com.example.airbnb.business.web.controller.member.dto.MemberWishListResponse;
+import com.example.airbnb.business.web.service.member.MemberReadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberReadService memberReadService;
+
+    @GetMapping("/{githubId}")
+    public MemberWishListResponse searchMemberWishList(@PathVariable String githubId) {
+        return memberReadService.findWishes(githubId);
+    }
 }
