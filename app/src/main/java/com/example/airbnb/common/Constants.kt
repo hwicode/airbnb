ec9b999316f376340eadb7cffb9bb2190644da4b
Linus <zerolecture0922@gmail.com>
2022-05-31 16:39:55 +0900

    feat : 가격 그래프 화면 구현 완료 - 라이브러리를 활용하여 차트를 가진 가격 범위 그래프 구현 - 선 그래프를 막대 그래프로 변환하는 로직 구현 - 각 가격 범위에 맞게 TextView의 Text 변경

--- app/src/main/java/com/example/airbnb/common/Constants.kt
+++ app/src/main/java/com/example/airbnb/common/Constants.kt
@@ -3,8 +3,16 @@ package com.example.airbnb.common
 object Constants {
     const val SUNDAY_DAY_OF_WEEK = 7
     const val CALENDAR_IN_RANGE_COLOR= 0xFFF3F5F7.toInt()
+
     var JSESSIONID = ""
     const val WEBVIEW_LOGIN_URL = "http://3.34.207.233:8080/login"
+
+    const val PRICE_MAX_VALUE = 20
+    const val PRICE_MIN_VALUE = 0
+    const val SEEKBAR_VALUE_GAP = 0.8f
+    const val SEEKBAR_VACANT_GAP = 0.2f
+    const val SEEKBAR_VACANT_VALUE = 0.0f
+    const val OMAN_WON = 50000
 }
 
 enum class DayOfMonthRange(val range: IntRange) {
