9e8ce1785ed838c98b18bf4bb5ca9e3c8b45ee38
hwicode <khc3796@naver.com>
2022-05-31 11:43:28 +0900

    Feat: ReservationController 생성
    - ReservationService를 ReservationReadService로 이름 수정

--- Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationController.java
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationController.java
@@ -1,14 +1,23 @@
 package com.example.airbnb.business.web.controller.reservation;
 
-import com.example.airbnb.business.web.service.reservation.ReservationService;
+import com.example.airbnb.business.web.controller.reservation.dto.ReservationResponse;
+import com.example.airbnb.business.web.service.reservation.ReservationReadService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/reservation")
 @RequiredArgsConstructor
 public class ReservationController {
 
-    private final ReservationService reservationService;
+    private final ReservationReadService reservationReadService;
 
+    @GetMapping("id")
+    public ReservationResponse searchReservationDetail(@PathVariable("id") Long id) {
+        return reservationReadService.findByReservationId(id);
+    }
 
 }
---
+++ Backend/src/main/java/com/example/airbnb/business/web/service/reservation/ReservationReadService.java
@@ -0,0 +1,13 @@
+package com.example.airbnb.business.web.service.reservation;
+
+import com.example.airbnb.business.core.repository.reservation.ReservationRepository;
+import com.example.airbnb.business.web.controller.reservation.dto.ReservationResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationReadService {
+
+    public ReservationResponse findByReservationId(Long id) {
+
+    }
+}
