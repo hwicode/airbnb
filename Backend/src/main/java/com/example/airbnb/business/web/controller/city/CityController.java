70d38f9f2d57833b1943ce2366c863ad8ed1975a
Jun <92818747+devjun10@users.noreply.github.com>
2022-06-01 14:15:37 +0900

    Feat: 근처 도시 조회 hwicode/airbnb#50
    - 반환된 도시 정보 DTO로 변환
    - DTO 필드 추가

--- Backend/src/main/java/com/example/airbnb/business/web/controller/city/CityController.java
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/city/CityController.java
@@ -1,11 +1,13 @@
 package com.example.airbnb.business.web.controller.city;
 
-import com.example.airbnb.business.web.controller.accommodation.dto.AccommodationCitiesResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/accommodations")
@@ -15,7 +17,10 @@ public class CityController {
     private final CityService cityService;
 
     @GetMapping("/cities/{cityName}")
-    public NearByCitesResponse searchAccommodations(@PathVariable String cityName) {
-        return new NearByCitesResponse(cityService.findNearByCitiesBy(cityName));
+    public List<NearByCity> searchAccommodations(@PathVariable String cityName) {
+        return cityService.findNearByCitiesBy(cityName)
+                .stream()
+                .map(NearByCity::new)
+                .collect(Collectors.toList());
     }
 }
--- Backend/src/main/java/com/example/airbnb/business/web/controller/city/NearByCity.java
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/city/NearByCity.java
@@ -1,5 +1,20 @@
 package com.example.airbnb.business.web.controller.city;
 
+import com.example.airbnb.business.core.domain.accommodation.City;
+import lombok.Getter;
+
+@Getter
 public class NearByCity {
 
+    private String name;
+    private String image;
+    private Double latitude;
+    private Double longitude;
+
+    public NearByCity(City city) {
+        this.name = city.getName();
+        this.image = city.getImage();
+        this.latitude = city.getLatitude();
+        this.longitude = city.getLongitude();
+    }
 }
