61d4467f1e73c23c59205ebed7bd08f5683b53be
ramkid91 <ramkid91@gmail.com>
2022-06-01 18:20:38 +0900

    feat:  게스트 인원수 페이지 uI 완성,
    - GuestSelector와 Divider을 이용해 인원 선택 UI 구현
    - To DO: 버튼 클릭 로직 , 클릭에 따른 상단 인원수 변경,  건너뛰기,지우기 로직 적용 필요

--- android/app/src/main/java/com/example/airbnb/ui/guestRange/GuestRangeFragment.kt
+++ android/app/src/main/java/com/example/airbnb/ui/guestRange/GuestRangeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -14,16 +16,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.airbnb.R
 
-
 class GuestRangeFragment : Fragment() {
-
+    private lateinit var skipBtn: Button
+    private lateinit var nextBtn: ImageButton
+    private lateinit var navigator: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +42,19 @@ class GuestRangeFragment : Fragment() {
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        skipBtn = view.rootView.findViewById(R.id.btn_information_skip)
+        nextBtn = view.rootView.findViewById(R.id.iBtn_information_next)
+        navigator= Navigation.findNavController(view)
+
+        nextBtn.setOnClickListener {
+            navigator.navigate(R.id.action_guestRangeFragment_to_calendarFragment)
+        }
+        skipBtn.setOnClickListener {
+            navigator.navigate(R.id.action_guestRangeFragment_to_calendarFragment)
+        }
+    }
 }
 
 @Composable
@@ -50,7 +66,7 @@ fun GuestRangeView() {
     ) {
         Column() {
             GuestInputHeader(input = "인원 입력", guest = "게스트 0", child = "유아 0")
-            GuestSelector("성인", "만 13세 이상")
+            GuessSelectorLayout()
         }
     }
 
@@ -73,39 +89,68 @@ fun GuestInputHeader(input: String, guest: String, child: String) {
 
 @Composable
 fun GuestSelector(type: String, ageRange: String) {
+    var countNumber=0
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
+            .padding(bottom = 16.dp)
+        ,
     ) {
-        Spacer(modifier = Modifier.width(16.dp))
-        Column {
+        Column(modifier = Modifier.fillMaxWidth(0.5F)) {
+
             Spacer(modifier = Modifier.height(12.dp))
             Text(text = type, style = MaterialTheme.typography.subtitle1)
-            Text(text = ageRange, style = MaterialTheme.typography.subtitle2, color = Color(0xFF828282))
+            Text(
+                text = ageRange,
+                style = MaterialTheme.typography.subtitle2,
+                color = Color(0xFF828282)
+            )
         }
-        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
-            var countNumber: Int = 0
-            Spacer(modifier = Modifier.height(21.dp))
-            IconButton(onClick = { /*TODO*/ }) {
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .fillMaxWidth()
+                .wrapContentHeight(Alignment.Top)
+        ) {
+            IconButton(enabled = countNumber != 0, onClick = { countNumber -= 1 }) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_remove_circle_outline_black_24dp),
                     contentDescription = "minus_selector_btn",
-                    modifier = Modifier.size(30.dp)
+                    modifier = Modifier.size(35.dp)
                 )
             }
-            Text(text = "$countNumber", modifier = Modifier.wrapContentSize(Alignment.Center))
-            IconButton(onClick = { /*TODO*/ }) {
+            Text(
+                text = "$countNumber",
+                modifier = Modifier.wrapContentSize(Alignment.Center),
+                style = MaterialTheme.typography.h6
+            )
+            IconButton(onClick = { countNumber+=1})
+            {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_add_circle_outline_black_24dp),
                     contentDescription = "plus_selector_btn",
-                    modifier = Modifier.size(30.dp)
+                    modifier = Modifier.size(35.dp)
                 )
             }
+
         }
     }
 }
 
+@Composable
+fun GuessSelectorLayout(){
+    Column(modifier = Modifier.wrapContentHeight().padding(start=16.dp, end=16.dp)) {
+        GuestSelector(type = "성인", ageRange = "만13세이상")
+        Divider(modifier = Modifier.fillMaxWidth().height(1.dp), color = Color(0xFFE0E0E0 ))
+        GuestSelector(type = "어린이", ageRange = "만2~13세")
+        Divider(modifier = Modifier.fillMaxWidth().height(1.dp), color = Color(0xFFE0E0E0 ))
+        GuestSelector(type = "유아", ageRange = "만2세 미만")
+    }
+}
 
 @Composable
 @Preview(showBackground = true)
