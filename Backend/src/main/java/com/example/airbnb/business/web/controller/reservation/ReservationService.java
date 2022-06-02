1c76ecd6a7b2daf68080b43c37ac4bbe84d2173b
Jun <92818747+devjun10@users.noreply.github.com>
2022-06-02 13:33:22 +0900

    Feat: 숙소 조회 및 예약 생성 추가 hwicode/airbnb#53
    - 숙소 조회 및 예약 엔티티 생성

--- Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationService.java
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationService.java
@@ -1,5 +1,13 @@
 package com.example.airbnb.business.web.controller.reservation;
 
+import com.example.airbnb.business.core.domain.accommodation.Accommodation;
+import com.example.airbnb.business.core.domain.accommodation.AccommodationType;
+import com.example.airbnb.business.core.domain.reservation.Reservation;
+import com.example.airbnb.business.core.repository.accommodation.AccommodationRepository;
+import com.example.airbnb.business.core.repository.reservation.ReservationRepository;
+import com.example.airbnb.business.web.service.accommodation.AccommodationReadService;
+import com.example.airbnb.common.exception.BusinessException;
+import com.example.airbnb.common.exception.accommodation.AccommodationTypeException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,7 +19,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReservationService {
 
+    private AccommodationRepository accommodationRepository;
+    private ReservationRepository reservationRepository;
+
     public List<AccommodationReservationResponse> reservation(Long accommodationId, AccommodationReservationRequest request) {
+        Accommodation findAccommodation = accommodationRepository.findById(accommodationId)
+                .orElseThrow(() -> new BusinessException(AccommodationTypeException.ACCOMMODATION_NOT_FOUND));
+
+        Reservation reservation = new Reservation();
         return null;
     }
 }
