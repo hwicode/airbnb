3e33413a0b62554a23b81f3248b46d928ac3f4ca
hwicode <khc3796@naver.com>
2022-06-05 15:27:37 +0900

    Feat: 위시리스트에 숙소 추가 API 기능 구현(MemberService에 기능 추가) hwicode/airbnb#50
    - AccommodationRepository에서 EntityGraph 애노테이션 제거

--- Backend/src/main/java/com/example/airbnb/business/core/repository/accommodation/AccommodationRepository.java
+++ Backend/src/main/java/com/example/airbnb/business/core/repository/accommodation/AccommodationRepository.java
@@ -2,7 +2,6 @@ package com.example.airbnb.business.core.repository.accommodation;
 
 import com.example.airbnb.business.core.domain.accommodation.Accommodation;
 import io.lettuce.core.dynamic.annotation.Param;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,8 +10,6 @@ import java.util.Optional;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
 
-
-    @EntityGraph(attributePaths = {"member", "images"})
     Optional<Accommodation> findByAccommodationId(Long id);
 
     @Query("select a from Accommodation a where FUNCTION('ST_Distance_Sphere', a.location.point, function('ST_GeomFromText', :point)) <= :searchRange")
