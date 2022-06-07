699c1ecc8fe039ee39e2e63497ae90c715864da7
ramkid91 <ramkid91@gmail.com>
2022-06-08 08:48:49 +0900

    feat:  지도위에 표시될 리사이클러뷰 , 어댑터 구현
    - CardView로 아이템 전체를 감싸는 형태
    - 기존 search_result_accommodation 아이템, 어댑터 수정해서 사용
    - 아이템 클릭시 디테일로 이동할 수있도록 콜백 정의( 이동 함수는 미구현)

--- android/app/src/main/java/com/example/airbnb/ui/mapSearch/MapSearchActivity.kt
+++ android/app/src/main/java/com/example/airbnb/ui/mapSearch/MapSearchActivity.kt
@@ -1,84 +1,64 @@
 package com.example.airbnb.ui.mapSearch
 
 import android.graphics.Canvas
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
-import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 import com.example.airbnb.BuildConfig
 import com.example.airbnb.R
-import com.example.airbnb.databinding.ActivityMainBinding
+import com.example.airbnb.domain.model.SearchResultAccommodation
 import com.example.airbnb.ui.common.TextDrawable
-import com.skt.Tmap.TMapData
 import com.skt.Tmap.TMapMarkerItem
 import com.skt.Tmap.TMapPoint
 import com.skt.Tmap.TMapView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 
 class MapSearchActivity : AppCompatActivity() {
 
     private lateinit var tMapView: TMapView
-
+    private lateinit var cardRecyclerView:RecyclerView
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_search)
-
         tMapView = findViewById<TMapView>(R.id.tmap)
+        tMapMarkerTest()
+        cardRecyclerView= findViewById(R.id.rv_map_card_list)
         tMapView.setSKTMapApiKey(BuildConfig.TMAP_KEY)
-        val markItem = TMapMarkerItem()
-        val markItem2 = TMapMarkerItem()
-        val markItem3 = TMapMarkerItem()
-        val tMapPoint1 = TMapPoint(37.570841, 126.985302)
-        val tMapPoint2 = TMapPoint(37.570842, 126.995303)
-        val tMapPoint3 = TMapPoint(37.580843, 126.985304)
-        val textDrawable = TextDrawable("$23,000")
-        val textDrawable2 = TextDrawable("$230,000")
-        val textDrawable3 = TextDrawable("$2,300,000")
-        textDrawable.draw(Canvas())
-        textDrawable2.draw(Canvas())
-        textDrawable3.draw(Canvas())
+        val adapter= MapSearchAccommodationCardAdapter(){
+            openDetail()
+        }
+        cardRecyclerView.adapter= adapter
+        adapter.submitList(getSearchResultAccommodations())
 
+    }
+
+    private fun tMapMarkerTest(){
+        val markItem = TMapMarkerItem()
+        val tMapPoint1 = TMapPoint(37.570841, 126.985302)
+        val textDrawable = TextDrawable("$23,000")
+        textDrawable.draw(Canvas())
         markItem.icon = textDrawable.toBitmap(200, 100)
         markItem.tMapPoint = tMapPoint1
-        markItem2.icon = textDrawable2.toBitmap(200, 100)
-        markItem2.tMapPoint = tMapPoint2
-        markItem3.icon = textDrawable3.toBitmap(200, 100)
-        markItem3.tMapPoint = tMapPoint3
-        tMapView.addMarkerItem("markItem2", markItem2)
-        tMapView.addMarkerItem("markItem3", markItem3)
-        findViewById<TMapView>(R.id.tmap).addMarkerItem("markItem1", markItem)
-        findViewById<TMapView>(R.id.tmap).setCenterPoint(126.985302, 37.570841)
+        tMapView.addMarkerItem("markItem1", markItem)
+        tMapView.setCenterPoint(126.985302, 37.570841)
+    }
 
-        val first = TMapPoint(37.570841, 126.985302)
-        val second = TMapPoint(37.580843, 126.985304)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val tMapPolyLine = TMapData().findPathData(first, second)
-                tMapPolyLine.lineColor = Color.BLUE
-                tMapPolyLine.lineWidth = 2f
-                println("거리 ${tMapPolyLine.distance}")
-                val test = TMapData().findPathDataAllType(TMapData.TMapPathType.CAR_PATH, first, second)
-                tMapView.addTMapPolyLine("line1", tMapPolyLine)
-                println(test.textContent)
-                println(test.toString())
-                println(test.documentURI)
-                println(test.documentElement)
-                println(test.nodeValue)
-            } catch (e: Exception) {
-                println(e)
-                e.printStackTrace()
-            }
+    // SearchResultFragment -> MapSearchActivity로 이동할때 전달받는다 가정
+    private fun getSearchResultAccommodations():List<SearchResultAccommodation>{
+        val accommodations = mutableListOf<SearchResultAccommodation>()
+        for(i in 0..100){
+            accommodations.add(SearchResultAccommodation("dummy Image", true,"서울 왕십리 게스트하우스"))
         }
+        return accommodations
+    }
 
-
+    private fun openDetail(){
+        //navigate to detail
     }
 }
 
