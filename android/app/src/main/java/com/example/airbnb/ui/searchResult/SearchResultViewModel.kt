1cd0b336acfd3e042f4225c0bd1ec4ffb4322094
ramkid91 <ramkid91@gmail.com>
2022-06-08 11:19:21 +0900

    feat: 검색 결과 무한 스크롤 적용
    - viewType의 프로그래스바와 아이템 뷰 구분
    - recylcerview의 scrollListener 등록
    - 한 페이지에 10개의 아이템이 보이도록 구현
    - canScrollVertically를 통해 최하단에 도달했는지 판단

--- android/app/src/main/java/com/example/airbnb/ui/searchResult/SearchResultViewModel.kt
+++ android/app/src/main/java/com/example/airbnb/ui/searchResult/SearchResultViewModel.kt
@@ -1,15 +1,21 @@
 package com.example.airbnb.ui.searchResult
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.airbnb.domain.model.SearchResult
 import com.example.airbnb.domain.model.SearchResultAccommodation
 
 class SearchResultViewModel:ViewModel() {
 
-    fun makeDummySearchResultList(): List<SearchResultAccommodation> {
-        val accommodations = mutableListOf<SearchResultAccommodation>()
-        for(i in 0..100){
+    private val _searchResult= MutableLiveData<List<SearchResult>>()
+    val searchResult:LiveData<List<SearchResult>> = _searchResult
+
+    fun makeDummySearchResultList() {
+        val accommodations = mutableListOf<SearchResult>()
+        for(i in 0..10){
             accommodations.add(SearchResultAccommodation("dummy Image", true,"서울 왕십리 게스트하우스"))
         }
-        return accommodations
+        _searchResult.value= accommodations
     }
 }
