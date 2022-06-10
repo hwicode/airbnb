a5517c76539045020ece40b8d0c1031bb3b344c0
Jun <92818747+devjun10@users.noreply.github.com>
2022-06-10 09:18:44 +0900

    Feat: 예약 조회 서비스 및 컨트롤러 수정
    - 예약 상세조회 및 예약 리스트 아이디로 보도록 수정

--- Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationController.java
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/reservation/ReservationController.java
@@ -1,23 +1,46 @@
 package com.example.airbnb.business.web.controller.reservation;
 
+import com.example.airbnb.business.web.controller.reservation.dto.AccommodationReservationRequest;
+import com.example.airbnb.business.web.controller.reservation.dto.AccommodationReservationResponse;
+import com.example.airbnb.business.web.controller.reservation.dto.DetailedReservationResponse;
 import com.example.airbnb.business.web.controller.reservation.dto.ReservationResponse;
 import com.example.airbnb.business.web.service.reservation.ReservationReadService;
+import com.example.airbnb.common.login.token.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Slf4j
 @RestController
-@RequestMapping("/reservations")
+@RequestMapping("/api/reservations")
 @RequiredArgsConstructor
 public class ReservationController {
 
+    private final ReservationService reservationService;
     private final ReservationReadService reservationReadService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/{id}")
-    public ReservationResponse searchReservationDetail(@PathVariable("id") Long id) {
+    public DetailedReservationResponse searchDetailedReservation(@PathVariable("id") Long id) {
         return reservationReadService.findByReservationId(id);
+    }
+
+    @GetMapping
+    public List<ReservationResponse> searchReservations(HttpServletRequest httpServletRequest) {
+        String githubId = jwtTokenProvider.getAttribute(httpServletRequest.getHeader("Authorization"));
+        log.info("숙소 조회: {}", githubId);
+        return reservationReadService.findReservationsBy(githubId);
+    }
+
+    @PostMapping
+    public AccommodationReservationResponse reserveAccommodation(HttpServletRequest httpServletRequest, @RequestParam("accommodationId") Long accommodationId,
+                                                                 @RequestBody AccommodationReservationRequest request) {
+        String githubId = jwtTokenProvider.getAttribute(httpServletRequest.getHeader("Authorization"));
+        log.info("숙소 조회: {}", githubId);
+        return new AccommodationReservationResponse(reservationService.reservation(githubId, accommodationId, request));
     }
 
 }
