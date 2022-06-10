31a28bc2d424683b1604b307e8ffe6a7693c04a3
Jun <92818747+devjun10@users.noreply.github.com>
2022-06-10 09:20:09 +0900

    Feat: 숙소 등록 클래스 추가 hwicode/airbnb#50
    - 숙소 등록 클래스들 추가

--- Backend/src/main/java/com/example/airbnb/business/web/service/accommodation/AccommodationReadService.java
+++ Backend/src/main/java/com/example/airbnb/business/web/service/accommodation/AccommodationReadService.java
@@ -1,30 +1,44 @@
 package com.example.airbnb.business.web.service.accommodation;
 
-import com.example.airbnb.business.core.domain.accommodation.AccommodationOptionLine;
-import com.example.airbnb.business.core.domain.accommodation.AmenitySubCategory;
-import com.example.airbnb.business.core.domain.accommodation.Comment;
-import com.example.airbnb.business.core.domain.accommodation.Image;
-import com.example.airbnb.business.core.repository.accommodation.querydsl.AccommodationReadRepository;
-import com.example.airbnb.business.core.repository.accommodation.querydsl.AmenityReadRepository;
-import com.example.airbnb.business.core.repository.accommodation.querydsl.CommentReadRepository;
-import com.example.airbnb.business.core.repository.accommodation.querydsl.ImageReadRepository;
-import com.example.airbnb.business.web.controller.accommodation.dto.AccommodationResponse;
+import com.example.airbnb.business.core.domain.accommodation.*;
+import com.example.airbnb.business.core.repository.city.CityRepository;
+import com.example.airbnb.business.core.repository.accommodation.read.AccommodationReadRepository;
+import com.example.airbnb.business.core.repository.accommodation.read.AmenityReadRepository;
+import com.example.airbnb.business.core.repository.accommodation.read.CommentReadRepository;
+import com.example.airbnb.business.core.repository.accommodation.read.ImageReadRepository;
+import com.example.airbnb.business.web.controller.accommodation.dto.*;
 import com.example.airbnb.common.exception.BusinessException;
 import com.example.airbnb.common.exception.accommodation.AccommodationTypeException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import static com.example.airbnb.common.utils.PageUtils.PAGE_SIZE;
+import static java.lang.Boolean.FALSE;
+import static java.util.Collections.EMPTY_LIST;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AccommodationReadService {
+
+    private final CityRepository cityRepository;
 
     private final AccommodationReadRepository accommodationReadRepository;
     private final ImageReadRepository imageReadRepository;
     private final AmenityReadRepository amenityReadRepository;
     private final CommentReadRepository commentReadRepository;
+    private final TagReadRepository tagReadRepository;
 
     @Transactional(readOnly = true)
     public AccommodationResponse findByAccommodationId(Long id) {
@@ -32,11 +46,46 @@ public class AccommodationReadService {
                 .orElseThrow(() -> new BusinessException(AccommodationTypeException.ACCOMMODATION_NOT_FOUND));
 
         List<Image> images = imageReadRepository.findImagesByAccommodation(id);
-        List<AmenitySubCategory> amenitySubCategories = amenityReadRepository.findAmenityCategoriesByAccommodation(id);
+        List<Amenity> amenities = amenityReadRepository.findAmenityCategoriesByAccommodation(id);
         List<Comment> comments = commentReadRepository.findCommentsByAccommodation(id);
         List<AccommodationOptionLine> accommodationOptionLines = accommodationReadRepository.findAccommodationOptionLinesByAccommodation(id);
 
-        accommodationResponse.add(images, amenitySubCategories, comments, accommodationOptionLines);
+        accommodationResponse.add(images, amenities, comments, accommodationOptionLines);
+
         return accommodationResponse;
+    }
+
+    @Transactional(readOnly = true)
+    public Slice<AccommodationRelatedCityResponse> findByAccommodationsByCityName(String cityName, int page) {
+        Optional<City> findCity = cityRepository.findCityByName(cityName);
+        if (findCity.isPresent()) {
+            Slice<AccommodationRelatedCityResponse> accommodations = accommodationReadRepository.findByAccommodationsByCityId(findCity.get().getCityId(), PageRequest.of(page, 10));
+            log.info("도시 이름으로 숙소 검색. 검색된 도시 {}", cityName);
+            return accommodations;
+        }
+        return new SliceImpl<AccommodationRelatedCityResponse>(EMPTY_LIST, PageRequest.of(0, 1), FALSE);
+    }
+
+    @Transactional(readOnly = true)
+    public List<SearchPriceResponse> findAccommodationPriceRangeByTagAndPeriod(String tag, LocalDate checkIn, LocalDate checkOut) {
+        if (checkIn == null || checkOut == null) {
+            Set<Long> accommodations = tagReadRepository.getAccommodationsByTag(tag);
+            return accommodationReadRepository.findAccommodationPricesById(accommodations);
+        }
+        Set<Long> accommodations = tagReadRepository.getAccommodationsByTag(tag);
+        return tagReadRepository.findAccommodationPriceRangeByTagAndPeriod(accommodations, checkIn, checkOut);
+    }
+
+    @Transactional(readOnly = true)
+    public Slice<AccommodationSearchResponse> searchAccommodations(AccommodationSearchCondition searchCondition, int page) {
+        String tag = searchCondition.getTagName();
+        LocalDate checkIn = searchCondition.getCheckIn();
+        LocalDate checkOut = searchCondition.getCheckOut();
+
+        List<Long> accommodationIds = findAccommodationPriceRangeByTagAndPeriod(tag, checkIn, checkOut).stream()
+                .map(SearchPriceResponse::getAccommodationId)
+                .collect(Collectors.toList());
+
+        return accommodationReadRepository.findAccommodationsByCondition(searchCondition, accommodationIds, PageRequest.of(page, PAGE_SIZE));
     }
 }
---
+++ Backend/src/main/java/com/example/airbnb/business/web/service/accommodation/AccommodationService.java
@@ -0,0 +1,40 @@
+package com.example.airbnb.business.web.service.accommodation;
+
+import com.example.airbnb.business.core.domain.accommodation.Accommodation;
+import com.example.airbnb.business.core.domain.accommodation.City;
+import com.example.airbnb.business.core.domain.member.Member;
+import com.example.airbnb.business.core.repository.accommodation.AccommodationRepository;
+import com.example.airbnb.business.core.repository.city.CityRepository;
+import com.example.airbnb.business.core.repository.member.MemberRepository;
+import com.example.airbnb.business.web.controller.accommodation.dto.AccommodationRegisterRequest;
+import com.example.airbnb.common.exception.BusinessException;
+import com.example.airbnb.common.exception.accommodation.CityTypeException;
+import com.example.airbnb.common.exception.member.MemberTypeException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AccommodationService {
+
+    private final AccommodationRepository accommodationRepository;
+    private final MemberRepository memberRepository;
+    private final CityRepository cityRepository;
+
+    @Transactional
+    public Accommodation registerAccommodation(String githubId, AccommodationRegisterRequest request) {
+        Member member = memberRepository.findByGithubId(githubId)
+                .orElseThrow(() -> new BusinessException(MemberTypeException.MEMBER_NOT_FOUND));
+
+        City city = cityRepository.findCityByName(request.getCityName())
+                .orElseThrow(() -> new BusinessException(CityTypeException.CITY_NOT_FOUND));
+
+        Accommodation accommodation = request.toEntity();
+        accommodation.addInformation(member, city);
+
+        return accommodationRepository.save(accommodation);
+    }
+}
