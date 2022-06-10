dfca42b35350f39eec5c8c3e2b24cf3a2b51232a
Jun <92818747+devjun10@users.noreply.github.com>
2022-06-10 09:13:50 +0900

    Refactor: 도시 전체 조회 hwicode/airbnb#70
    - 도시 조회 패키지 경로 수정

---
+++ Backend/src/main/java/com/example/airbnb/business/web/controller/city/CityController.java
@@ -0,0 +1,28 @@
+package com.example.airbnb.business.web.controller.city;
+
+import com.example.airbnb.business.web.controller.city.dto.NearByCity;
+import com.example.airbnb.business.web.service.city.CityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/cities")
+@RequiredArgsConstructor
+public class CityController {
+
+    private final CityService cityService;
+
+    @GetMapping
+    public List<NearByCity> getCities(HttpServletRequest request) {
+        return cityService.getCities()
+                .stream()
+                .map(NearByCity::new)
+                .collect(Collectors.toList());
+    }
+}
