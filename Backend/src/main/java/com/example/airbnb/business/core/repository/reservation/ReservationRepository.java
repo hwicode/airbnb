3bcc7c5a53d60c448354f37127188d2f4ec3b0d6
Jun <92818747+devjun10@users.noreply.github.com>
2022-06-10 09:08:03 +0900

    Refactor: 회원 레포지토리 수정 hwicode/airbnb#70
    - 회원 레포지토리 수정

--- Backend/src/main/java/com/example/airbnb/business/core/repository/reservation/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.airbnb.business.core.repository.reservation;
-
-import com.example.airbnb.business.core.domain.reservation.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
